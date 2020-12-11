df8b01f8ba0957fe371c2e24ef0f7d11c44cfa5b
Jason Herrera <baytownjason@yahoo.com>
2020-12-11 20:10:37 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -3,15 +3,11 @@ require 'pry'
 
 def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
-  binding.pry 
   puts 'hello'
 end
 
 def play_game(player1, player2)
   "#{player1} is better than #{player2}"
-  binding.pry
-  hi = 'hi'
 end
 
-start_game("Jason", "Astrid")
-play_game("Astrid", "Jason")
+
