cf047ea4cb1332b3374a0c4693a124a593a07796
Jason Herrera <baytownjason@yahoo.com>
2020-12-11 20:55:59 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -4,11 +4,8 @@ require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
     "ssssssssss" + string
-    puts 'Hello'
   else
     string
-    binding.pry
-    puts 'jekko'
   end
   binding.pry
   puts "almost done"
